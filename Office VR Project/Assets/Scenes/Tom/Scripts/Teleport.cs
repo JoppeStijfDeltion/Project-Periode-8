18abec907d7871cf081cb5817357b1f489540188
Tomvhe <tvhe@outlook.com>
2018-05-17 08:54:22 +0200

    Teleport

---
+++ Office VR Project/Assets/Scenes/Tom/Scripts/Teleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Teleport : MonoBehaviour {
+
+	public Transform hand;
+
+	private NavMeshAgent agent;
+
+	private void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	private void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			RaycastHit hit;
+			if (Physics.Raycast (hand.position, hand.forward, out hit, 10f)) {
+				print (hit.point);
+				agent.SetDestination (hit.point);
+			}
+		}
+	}
+}
