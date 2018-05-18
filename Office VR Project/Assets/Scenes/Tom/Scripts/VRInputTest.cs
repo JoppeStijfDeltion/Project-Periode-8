47bfa7809b6ea9a6d98be8bdce64e29c87284be3
Tomvhe <tvhe@outlook.com>
2018-05-18 14:50:14 +0200

    Added teleportation

--- Office VR Project/Assets/Scenes/Tom/Scripts/VRInputTest.cs
+++ Office VR Project/Assets/Scenes/Tom/Scripts/VRInputTest.cs
@@ -3,52 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VRInputTest : MonoBehaviour {
+    public bool showPrints;
 
-    [Header ("X = left, Y = right")]
-    public Vector2 trackpadVertical;
-    public Vector2 trackpadHorizontal;
-    public Vector2 triggerSqueeze;
-    public Vector2 gripButton;
+    private SteamVR_TrackedObject trackedObj;
 
-    public void Update () {
-        #region Buttons
-        // Menu
-        if (Input.GetButtonDown ("OpenVR_L_Button_Menu")) {
-            Debug.Log ("<color=blue>Menu</color>");
-        }
-        if (Input.GetButtonDown ("OpenVR_R_Button_Menu")) {
-            Debug.Log ("<color=red>Menu</color>");
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input ((int) trackedObj.index); }
+    }
+
+    private void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject> ();
+    }
+
+    private void Update () {
+        if (showPrints) {
+            if (Controller.GetAxis () != Vector2.zero) {
+                Debug.Log (gameObject.name + Controller.GetAxis ());
+            }
+
+            if (Controller.GetHairTriggerDown ()) {
+                Debug.Log (gameObject.name + " Trigger Press");
+            }
+
+            if (Controller.GetHairTriggerUp ()) {
+                Debug.Log (gameObject.name + " Trigger Release");
+            }
+
+            if (Controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+                Debug.Log (gameObject.name + " Grip Press");
+            }
+
+            if (Controller.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+                Debug.Log (gameObject.name + " Grip Release");
+            }
         }
 
-        // Trackpad Press
-        if (Input.GetButtonDown ("OpenVR_L_Button_Trackpad_Press")) {
-            Debug.Log ("<color=blue>Trackpad_Press</color>");
-        }
-        if (Input.GetButtonDown ("OpenVR_R_Button_Trackpad_Press")) {
-            Debug.Log ("<color=red>Trackpad_Press</color>");
-        }
-
-        // Trackpad Touch
-        if (Input.GetButtonDown ("OpenVR_L_Button_Trackpad_Touch")) {
-            Debug.Log ("<color=blue>Trackpad_Touch</color>");
-        }
-        if (Input.GetButtonDown ("OpenVR_R_Button_Trackpad_Touch")) {
-            Debug.Log ("<color=red>Trackpad_Touch</color>");
-        }
-
-        // Trigger Touch
-        if (Input.GetButtonDown ("OpenVR_L_Button_Trigger_Touch")) {
-            Debug.Log ("<color=blue>Trigger_Touch</color>");
-        }
-        if (Input.GetButtonDown ("OpenVR_R_Button_Trigger_Touch")) {
-            Debug.Log ("<color=red>Trigger_Touch</color>");
-        }
-        #endregion
-        #region Axes
-        trackpadVertical = new Vector2 (Input.GetAxis ("OpenVR_L_Axis_Trackpad_Vertical"), Input.GetAxis ("OpenVR_R_Axis_Trackpad_Vertical"));
-        trackpadHorizontal = new Vector2 (Input.GetAxis ("OpenVR_L_Axis_Trackpad_Horizontal"), Input.GetAxis ("OpenVR_R_Axis_Trackpad_Horizontal"));
-        triggerSqueeze = new Vector2 (Input.GetAxis ("OpenVR_L_Axis_Trigger_Squeeze"), Input.GetAxis ("OpenVR_R_Axis_Trigger_Squeeze"));
-        gripButton = new Vector2 (Input.GetAxis ("OpenVR_L_Axis_GripButton"), Input.GetAxis ("OpenVR_R_Axis_GripButton"));
-        #endregion
     }
 }
