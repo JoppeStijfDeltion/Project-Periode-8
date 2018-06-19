c9c75aea45b216be4ebaf40f0a658407bb84171a
Tomvhe <tvhe@outlook.com>
2018-06-19 16:21:29 +0200

    Added vent/screw system

--- Office VR Project/Assets/Scenes/Tom/Scripts/Vent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Vent : MonoBehaviour {
-
-	public int screws = 4;
-
-}
---
+++ Office VR Project/Assets/Vent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vent : MonoBehaviour {
+
+	public int numberOfScrews = 4;
+
+	public void Check () {
+		numberOfScrews--;
+		if (numberOfScrews <= 0) {
+			//Do something
+		}
+	}
+}
