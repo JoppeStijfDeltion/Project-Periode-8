5c776847f2a59db4458eee786faa37a09d20f1e5
Admin <guy_meulen@live.nl>
2018-06-26 16:32:47 +0200

    Last push

---
+++ Office VR Project/Assets/Scenes/Guy/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    
+    [Header("Settings:")]
+    public float rotSpeedX, rotSpeedY, rotSpeedZ;
+	
+	void Update () {
+        Rotate();
+	}
+
+    void Rotate()
+    {
+        transform.Rotate(rotSpeedX, rotSpeedY, rotSpeedZ);
+    }
+}
